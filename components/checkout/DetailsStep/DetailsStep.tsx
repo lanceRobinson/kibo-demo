69640b6f2d59e1f384df68ec803c19b5c4beaf91
lanceRobinson <96267470+lanceRobinson@users.noreply.github.com>
2026-05-12 10:24:59 -0400

    feat: prepopulate checkout email with logged-in user's email

--- components/checkout/DetailsStep/DetailsStep.tsx
+++ components/checkout/DetailsStep/DetailsStep.tsx
@@ -63,7 +63,7 @@ const DetailsStep = <T extends CrOrder | Checkout>(props: DetailsProps<T>) => {
 
   const { t } = useTranslation('common')
 
-  const { isAuthenticated } = useAuthContext()
+  const { isAuthenticated, user } = useAuthContext()
   const { showModal } = useModalContext()
   const {
     stepStatus,
@@ -74,7 +74,7 @@ const DetailsStep = <T extends CrOrder | Checkout>(props: DetailsProps<T>) => {
   } = useCheckoutStepContext()
 
   const personalDetails = {
-    email: checkout.email ?? '',
+    email: checkout.email || user?.emailAddress || '',
   }
 
   const openLoginModal = () => {
